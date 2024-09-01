fe4bf11b1980bedc93d3f1f9dc34754a13333579
robin <robin@robin-Inspiron-5758>
2024-09-01 20:07:48 +0100

    Continue with validate import

--- src/php-scripts/flow-model/import.php
+++ src/php-scripts/flow-model/import.php
@@ -1,10 +1,11 @@
 <?php
+include_once __DIR__ . '/../db-connect.php';
 
 header('Content-Type: application/json');
 
 $filenameItem = saveImportFile();
 $newPageArray = arrangePageData($filenameItem);
-if (validateImport($newPageArray)) {
+if (validateImportData($newPageArray, $_POST['username'])) {
     importPageData($newPageArray);
 }
 
@@ -203,16 +204,28 @@ function arrangePageData($filedata) {
     return $newModel();
 }
 
-function validateImportData(&$pageData) {
+function validateImportData(&$pageData, $username) {
     $message = "";
     $count = 0;
     foreach ($pageData as &$pageItem) {
         $page = $pageItem['page'];
         $message = validatePageDetails($page, $count);
         if ($message != "") break;
+        $message = validateUserAuthors($page, $username, $count);
+        if ($message != "") break;
+        $message = validateExternalAuthors($page, $count);
+        if ($message != "") break;
+        $message = validateReferences($page, $count);
+
         ++$count;
     }
     unset($pageItem);
+    if ($message != "") {
+        $response = ["result"=>false, "error"=>$message];
+        echo json_encode($response);
+        exit;
+    }
+    return true;
 }
 
 function validatePageDetails(&$page, $count) {
@@ -220,6 +233,10 @@ function validatePageDetails(&$page, $count) {
         $title = htmlspecialchars($page['title']);
         if ($title === "") {
             $message = "Missing Title at Page $count<br>";
+            return $message;
+        }
+        if (strlen($title) > 64) {
+            $message = "Title too long (>64 chars) at Page $count<br>";
             return $message;
         }
     }
@@ -248,14 +265,144 @@ function validatePageDetails(&$page, $count) {
 
     if (array_key_exists("description", $page)) {
         $description = htmlspecialchars($page['description']);
+        if (strlen($description) > 4096) {
+            $message = "Description too long (>4096 chars) at page $count, $title<br>";
+            return $message;
+        }
         $page['description'] = $description;
     }
     else {
         $page['description'] = "";
     }
+
+    if (array_key_exists("keywords", $page)) {
+        $keywords = htmlspecialchars($page['keywords']);
+        if (strlen($keywords) > 256) {
+            $message = "Keywords too long (>256 chars) at Page $count, $title<br>";
+            return $message;
+        }
+        $page['keywords'] = $keywords;
+    }
+    else {
+        $page['keywords'] = "";
+    }
+
+
     return "";
 }
 
+function validateUserAuthors(&$page, $username, $count) {
+    global $dbConn;
+
+    $message = "";
+    $gotUser = false;
+    if (!array_key_exists("user_authors", $page)) {
+        $page['user_authors'] = [$username];
+        return $message;
+    }
+    foreach ($page['user_authors'] as $user) {
+        $sql = "SELECT id FROM user WHERE username = ?";
+        $stmt = $dbConn->prepare($sql);
+        if (!$stmt) {
+            error_log("validateUserAuthors: problem with sql - {$dbConn->error}", 0);
+            $message = "Database error<br>";
+            return $message;
+        }
+        $stmt->bind_param("s", $user);
+        $result->execute();
+        if (!$result) {
+            error_log("validateUserAuthors: could not execute sql - {$dbConn->error}", 0);
+            $message = "Database error<br>";
+            return $message;
+        }
+        if ($result->num_rows === 0) {
+            $message = "Unidentified user in user authors of Page $count<br>";
+            return $message;
+        }
+        if ($user === $username) {
+            $gotUser = true;
+        }
+    }
+    if (!$gotUser) {
+        array_push($page['user_authors'], $username);
+    }
+    return $message;
+}
+
+function validateExternalAuthors(&$page, $count) {
+    $message = "";
+    if (!array_key_exists("external_authors", $page)) {
+        $page['external_authors'] = [];
+        return $message;
+    }
+    $index = 0;
+    foreach($page['external_authors'] as $author) {
+        $author = htmlspecialchars($author);
+        if (strlen($author) > 128) {
+            $message = "Author name length too long > 128 characters at page $count<br>";
+            return $message;
+        }
+        $page['external_authors'][$index] = $author;
+        ++$index;
+    }
+    return $message;
+}
+
+function validateReferences(&$page, $count) {
+    $message = "";
+    if (!array_key_exists($page, 'references')) {
+        $page['references'] = [];
+        return $message;
+    }
+    $index = 0;
+    foreach($page['references'] as $reference) {
+        if (!array_key_exists($reference, 'source')) {
+            $reference['source'] = "";
+        }
+        else {
+            $source = htmlspecialchars($reference['source']);
+            if (strlen($source) > 256) {
+                $message = "reference source > 256 chars long at page $count<br>";
+                return $message;
+            }
+            $reference['source'] = $source;
+        }
+
+        if (!array_key_exists($reference, $title)) {
+            $message = "Reference missing title at page $count<br>";
+            return $message;
+        }
+        $title = htmlspecialchars($reference['title']);
+        if ($title === "") {
+            $message = "Reference title is empty at page $count<br>";
+            return $message;
+        }
+        if (strlen($title) > 128) {
+            $message = "Reference title length > 128 at page $count<br>";
+            return $message;
+        }
+        $reference['title'] = $title;
+
+        if (!array_key_exists($reference, 'author')) {
+            $reference['author'] = "";
+        }
+        else {
+            $author = htmlspecialchars($reference['author']);
+            if (strlen($author) > 128) {
+                $message = "Reference author > 128 characters at page $count<br>";
+                return $message;
+            }
+            $reference['author'] = $author;
+        }
+
+        $page['references'][$index] = $reference;
+        ++$index;
+    }
+
+    return $message;
+
+}
+    
 function addNewModel($modelTitle) {
     global $dbConn;
 
